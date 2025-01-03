3d148145b80e48044e1f0bd48f7a48693e0f8f3d
walfihajar <wh.hajarwalfi@gmail.com>
2025-01-03 22:39:44 +0100

    Commit for change in pages/admin/layoutA.php

--- pages/admin/layoutA.php
+++ pages/admin/layoutA.php
@@ -9,21 +9,24 @@
 <body class="bg-gray-100">
     <div class="flex flex-col md:flex-row min-h-screen">
         <!-- Sidebar -->
-        <aside class="bg-gray-800 text-white w-full md:w-64 p-4 md:min-h-screen">
+        <aside class="bg-gray-800 text-white w-full md:w-44 p-4 md:min-h-screen">
             <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
             <nav>
                 <ul>
                     <li class="mb-2">
-                        <a href="?page=home" class="block p-2 hover:bg-gray-700 rounded">Home</a>
+                        <a href="../admin/statistiques.php" class="block p-2 hover:bg-gray-700 rounded">Statistics</a>
                     </li>
                     <li class="mb-2">
-                        <a href="?page=analytics" class="block p-2 hover:bg-gray-700 rounded">Analytics</a>
+                        <a href="../admin/manage_reservations.php" class="block p-2 hover:bg-gray-700 rounded">Reservations</a>
                     </li>
                     <li class="mb-2">
-                        <a href="?page=settings" class="block p-2 hover:bg-gray-700 rounded">Settings</a>
+                        <a href="../Admin/manage_vehicules.php" class="block p-2 hover:bg-gray-700 rounded">Vehicules</a>
                     </li>
                     <li class="mb-2">
-                        <a href="?page=profile" class="block p-2 hover:bg-gray-700 rounded">Profile</a>
+                        <a href="../admin/manage__reviews.php" class="block p-2 hover:bg-gray-700 rounded">Reviews</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="../admin/manage_categories.php" class="block p-2 hover:bg-gray-700 rounded">Categories</a>
                     </li>
                 </ul>
             </nav>
@@ -31,7 +34,9 @@
 
         <!-- Main Content -->
         <main class="flex-grow p-4">
-            
+            <?php
+                echo $content;
+            ?>
         </main>
     </div>
 </body>
