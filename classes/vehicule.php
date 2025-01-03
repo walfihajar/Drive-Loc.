f019f891952a918f8f4025dbd5855c8c31263947
walfihajar <wh.hajarwalfi@gmail.com>
2025-01-03 22:39:43 +0100

    Commit for change in classes/vehicule.php

--- classes/vehicule.php
+++ classes/vehicule.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('../config.php');
+require_once ('../../config.php');
 
 class Vehicule 
 {
@@ -18,21 +18,37 @@ class Vehicule
         $this->id_category = $id_category;
         $this->price = $price;
         $this->availability = $availability;
-        $thsi->image = $image;
+        $this->image = $image;
 
         $conn = Database :: getInstance();
         $this->db = $conn->getConnection();
     }
 
-    static public function show($db)
+    static public function countAll($db)
     {
-        $query = "SELECT v.id_vehicule, v.model, c.name, c.description, v.price, v.image
-                  FROM vehicule AS v
-                  INNER JOIN category AS c ON v.id_category = c.id_category
-                  WHERE v.availability = '1'
+        $query = "SELECT COUNT(*)
+                  FROM  vehicule
+                  WHERE availability ='1'
                 ";
 
         $stmt = $db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchColumn();
+        return $result;
+    }
+
+    static public function show($db,$limit = null,$offset= null)
+    {
+        $query = "SELECT v.id_vehicule, v.id_category, v.model, c.name, c.description, v.price, v.image
+                  FROM vehicule AS v
+                  INNER JOIN category AS c ON v.id_category = c.id_category
+                  WHERE v.availability = '1'
+                  LIMIT :limit OFFSET :offset
+                ";
+
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':limit',$limit,PDO::PARAM_INT);
+        $stmt->bindParam(':offset',$offset,PDO::PARAM_INT);
         $stmt->execute();
         $result = $stmt->FETCHALL(PDO::FETCH_OBJ);
         return $result;
@@ -40,21 +56,21 @@ class Vehicule
 
     static public function add($newVeh)
     {
-        $query = " INSERT INTO vehicule (model, id_category, price,availability,image)
-                   VALUES (:model, :id_category, :price, :availability, :image)
-                 ";
+        $query = "INSERT INTO vehicule (model, id_category, price, availability, image)
+                VALUES (:model, :id_category, :price, :availability, :image)";
 
         $stmt = $newVeh->db->prepare($query);
 
-        $stmt->bindParam(':id_user',$newVeh->model);
-        $stmt->bindParam(':id_vehicule',$newVeh->id_category);
-        $stmt->bindParam(':start_date',$newVeh->price);
-        $stmt->bindParam(':end_date',$newVeh->availability);
-        $stmt->bindParam(':pickup_address',$newVeh->image);
+        $stmt->bindParam(':model', $newVeh->model);
+        $stmt->bindParam(':id_category', $newVeh->id_category);
+        $stmt->bindParam(':price', $newVeh->price);
+        $stmt->bindParam(':availability', $newVeh->availability);
+        $stmt->bindParam(':image', $newVeh->image);
 
         $result = $stmt->execute();
         return $result;
     }
+
 
     public function delete($id_vehicule)
     {
